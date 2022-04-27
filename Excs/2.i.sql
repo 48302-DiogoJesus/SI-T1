f4e97985d28fca2dbbf6d45a85a2faad3dd435dc
Diogo Jesus <72945621+48302-DiogoJesus@users.noreply.github.com>
2022-04-27 19:50:42 +0100

    Update 2.i.sql

--- Excs/2.i.sql
+++ Excs/2.i.sql
@@ -1,17 +1,16 @@
 /*
  * 2.i)
- * Criar uma vista, que liste todos os alarmes. A vista deve apresentar a matrícula do veículo, o
+ * Criar uma vista, que liste todos os alarmes. A vista deve apresentar a matrÃ­cula do veÃ­culo, o
  * nome do condutor, as coordenadas e a data/hora do alarme;
  * */
 
 drop view list_all_alarmes;
 
 create or replace view list_all_alarmes as (
-	select 
-		v.matricula, v.nome_condutor, concat(r.latitude, ', ', r.longitude) as coordenadas,
-		r.marca_temporal::date as data,
-		r.marca_temporal::time as hora 
-	from
-		alarme as a inner join veiculo as v on a.id_veiculo = v.matricula
-					inner join registo as r	on a.id_registo = r.id
+    select 
+        v.matricula, v.nome_condutor, r.latitude, r.longitude,
+        r.marca_temporal
+    from
+        alarme as a inner join veiculo as v on a.id_veiculo = v.matricula
+                    inner join registo as r    on a.id_registo = r.id
 );
